691a2f52ed153b526b7843232ff0653e8f8dd3d0
jsmolka <jsmolka@pm.me>
2020-04-21 20:28:28 +0200

    add custom dark colors, rework index page

--- assets/scss/tailwind.config.js
+++ assets/scss/tailwind.config.js
@@ -1,12 +1,34 @@
 module.exports = {
   theme: {
     fontFamily: {
-      body: ['Inter', 'system-ui', '-apple-system', 'sans-serif']
+      body: [
+        'Inter',
+        'system-ui',
+        '-apple-system',
+        'sans-serif'
+      ]
     },
-    extend: {}
+    extend: {
+      colors: {
+        dark: {
+          100: '#EAEAEB',
+          200: '#CACCCD',
+          300: '#AAADAF',
+          400: '#6B6F73',
+          500: '#2B3137',
+          600: '#272C32',
+          700: '#1A1D21',
+          800: '#131619',
+          900: '#0D0F11'
+        }
+      }
+    }
   },
   variants: {
-    margin: ['responsive', 'first']
+    margin: [
+      'first',
+      'responsive'
+    ]
   },
   plugins: []
 }
