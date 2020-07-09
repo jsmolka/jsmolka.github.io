81f091476475da73e2c59b9e2f1ae7849ac3475a
jsmolka <jsmolka@pm.me>
2020-07-09 21:18:26 +0200

    update icon, add random stagger effect

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,23 @@
-const timeline = new TimelineMax();
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
-timeline.staggerFrom('rect', 1, {
-  scale: 0,
-  rotation: 90,
-  transformOrigin: 'center center',
-  ease: Power3.easeIn
-}, 0.01);
+function animate() {
+  const element = document.getElementById('index-icon');
+  element.classList.toggle('hidden', false);
+
+  const rectangles = Array.from(document.getElementsByTagName('rect'));
+  shuffle(rectangles);
+
+  const timeline = new TimelineMax();
+  timeline.staggerFrom(rectangles, 0.5, {
+    y: 16,
+    scale: 0,
+    ease: Quad.easeInOut
+  }, 0.01);
+}
+
+animate();
