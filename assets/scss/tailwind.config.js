cd8906133252067fe1e2742cf9de4d964163cb6c
jsmolka <jsmolka@pm.me>
2020-07-10 21:18:27 +0200

    use lowercase for colors

--- assets/scss/tailwind.config.js
+++ assets/scss/tailwind.config.js
@@ -33,52 +33,36 @@ module.exports = {
           900: '#212121'
         },
         // Polar Night
-        "nord-0": "#2E3440",
-        "nord-1": "#3B4252",
-        "nord-2": "#434C5E",
-        "nord-3": "#4C566A",
-        "polar-night-0": "#2E3440",
-        "polar-night-1": "#3B4252",
-        "polar-night-2": "#434C5E",
-        "polar-night-3": "#4C566A",
+        "polar-night-0": "#2e3440",
+        "polar-night-1": "#3b4252",
+        "polar-night-2": "#434c5e",
+        "polar-night-3": "#4c566a",
 
         // Snow Storm
-        "nord-4": "#D8DEE9",
-        "nord-5": "#E5E9F0",
-        "nord-6": "#ECEFF4",
-        "snow-storm-0": "#D8DEE9",
-        "snow-storm-1": "#E5E9F0",
-        "snow-storm-2": "#ECEFF4",
+        "snow-storm-0": "#d8dee9",
+        "snow-storm-1": "#e5e9f0",
+        "snow-storm-2": "#eceff4",
 
         // Frost
-        "nord-7": "#8FBCBB",
-        "nord-8": "#88C0D0",
-        "nord-9": "#81A1C1",
-        "nord-10": "#5E81AC",
-        "frost-0": "#8FBCBB",
-        "frost-1": "#88C0D0",
-        "frost-2": "#81A1C1",
-        "frost-3": "#5E81AC",
+        "frost-0": "#8fbcbb",
+        "frost-1": "#88c0d0",
+        "frost-2": "#81a1c1",
+        "frost-3": "#5e81ac",
 
         // Aurora
-        "nord-11": "#BF616A",
-        "nord-12": "#D08770",
-        "nord-13": "#EBCB8B",
-        "nord-14": "#A3BE8C",
-        "nord-15": "#B48EAD",
-        "aurora-0": "#BF616A",
-        "aurora-1": "#D08770",
-        "aurora-2": "#EBCB8B",
-        "aurora-3": "#A3BE8C",
-        "aurora-4": "#B48EAD",
+        "aurora-0": "#bf616a",
+        "aurora-1": "#d08770",
+        "aurora-2": "#ebcb8b",
+        "aurora-3": "#a3be8c",
+        "aurora-4": "#b48ead",
 
         // Base
         "base-dark": "#242933",
-        "base-light": "#F2F4F8",
+        "base-light": "#f2f4f8",
 
         // Grey
-        "grey-dark": "#ABB9CF",
-        "grey-light": "#7B88A1"
+        "grey-dark": "#abb9cf",
+        "grey-light": "#7b88a1"
       },
       borderWidth: {
         1: '1px',
