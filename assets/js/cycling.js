a19320c2fd00f778787ab3b3ae7aa17136eb5f1c
jsmolka <jsmolka@pm.me>
2021-02-05 22:51:59 +0100

    update strava, improve cycling

--- assets/js/cycling.js
+++ assets/js/cycling.js
@@ -1,17 +1,10 @@
 import moment from 'moment';
 import Chart from 'chart.js';
 
-(function() {
-  let chart = null;
-  let activities = null;
-
-  function round(value) {
-    return Math.round(value * 10) / 10;
-  }
-
+(async function() {
   const config = {
     type: 'line',
-    data:  {
+    data: {
       datasets: []
     },
     options: {
@@ -19,7 +12,7 @@ import Chart from 'chart.js';
         xAxes: [],
         yAxes: [{
           ticks: {
-            callback: function(value, index, values) {
+            callback: function(value) {
               return `${value} km`;
             }
           }
@@ -34,14 +27,12 @@ import Chart from 'chart.js';
         callbacks: {
           label: function(item, data) {
             const activity = data.datasets[item.datasetIndex].data[item.index].activity;
-            const duration = moment().startOf('day').add(activity.time, 'seconds');
-            const hours = activity.time / 3600;
 
             return [
-              `Distance: ${round(activity.distance)} km`,
-              `Moving time: ${duration.format(`${Math.trunc(hours)}:mm:ss`)}`,
-              `Average speed: ${round(activity.distance / hours)} km/h`,
-              `Elevation gain: ${round(activity.elevation)} m`
+              `Distance: ${activity.formatDistance}`,
+              `Moving time: ${activity.formatTime}`,
+              `Average speed: ${activity.formatAverage}`,
+              `Elevation gain: ${activity.elevation}`
             ];
           }
         }
@@ -49,157 +40,221 @@ import Chart from 'chart.js';
     }
   };
 
-  function groupBy(activities, format, date) {
-    const map = new Map();
-    for (const activity of activities) {
-      const key = activity.date.format(format);
+  class Activity {
+    constructor(date, time, distance, elevation) {
+      this.date = moment(date);
+      this.time = moment(time);
+      this.distance = distance;
+      this.elevation = elevation;
+    }
 
-      if (!map.has(key)) {
-        map.set(key, {
-          date: date(activity.date.clone()),
-          type: activity.type,
-          time: 0,
-          distance: 0,
-          elevation: 0
+    round(value) {
+      return Math.round(value * 10) / 10;
+    }
+
+    get formatTime() {
+      // Workaround to show more than 23 hours
+      const basis = moment().startOf('day').add(this.time, 'seconds');
+      const hours = Math.trunc(this.time / 3600);
+
+      return basis.format(`${hours}:mm:ss`);
+    }
+
+    get formatDistance() {
+      return `${this.round(this.distance)} km`;
+    }
+
+    get formatAverage() {
+      const hours = this.time / 3600;
+
+      return `${this.round(this.distance / hours)} km/h`;
+    }
+
+    get formatElevation() {
+      return `${this.round(this.elevation)} m`;
+    }
+
+    get titleDay() {
+      return this.date.format('MMM D, H:mm');
+    }
+
+    get titleWeek() {
+      return `${this.date.startOf('week').format('MMM D')} - ${this.date.endOf('week').format('MMM D')}`;
+    }
+
+    get titleMonth() {
+      return this.date.format('MMMM');
+    }
+  }
+
+  class Activities extends Array {
+    async fetch(url) {
+      return new Promise(resolve => {
+        const request = new XMLHttpRequest();
+
+        request.open('GET', url);
+        request.responseType = 'json';
+        request.onload = () => {
+          for (const datum of request.response) {
+            if (datum.type !== 'Ride') {
+              continue;
+            }
+
+            this.push(new Activity(
+              datum.date,
+              datum.time,
+              datum.distance / 1000,
+              datum.elevation
+            ));
+          }
+          resolve(this);
+        }
+        request.send();
+      });
+    }
+
+    groupBy(keyCallback, dateCallback) {
+      const map = new Map();
+      for (const activity of this) {
+        const key = keyCallback(activity);
+
+        if (!map.has(key)) {
+          map.set(key, new Activity(
+            dateCallback(activity),
+            0, 0, 0)
+          );
+        }
+
+        const value = map.get(key);
+        value.time += activity.time;
+        value.distance += activity.distance;
+        value.elevation += activity.elevation;
+      }
+      return Array.from(map.values());
+    }
+
+    groupByDay() {
+      return this.groupBy(
+        activity => activity.date.format('Y-w-D'),
+        activity => activity.date
+      );
+    }
+
+    groupByWeek() {
+      return this.groupBy(
+        activity => activity.date.format('Y-w'),
+        activity => activity.date.endOf('week')
+      );
+    }
+
+    groupByMonth() {
+      return this.groupBy(
+        activity => activity.date.format('Y-M'),
+        activity => activity.date.endOf('month')
+      );
+    }
+  }
+
+  class Dataset {
+    constructor(label, data) {
+      this.label = label;
+      this.borderColor = 'rgba(33, 150, 243, 1)';
+      this.backgroundColor = 'rgba(33, 150, 243, 0.1)';
+      this.borderWidth = 1;
+      this.lineTension = 0;
+      this.pointRadius = 5;
+      this.pointHitRadius = 5;
+      this.pointBorderColor = 'rgba(33, 150, 243, 1)';
+      this.pointBackgroundColor = 'rgb(233, 245, 254, 1)';
+      this.pointHoverRadius = 5;
+      this.data = data;
+    }
+  }
+
+  class Application {
+    constructor() {
+      this.chart = null;
+      this.config = Object.assign({}, config);
+      this.activities = new Activities();
+    }
+
+    async init() {
+      await this.activities.fetch('/static/data/strava.json');
+
+      this.update(
+        activity => activity.groupByDay(),
+        activity => activity.titleDay,
+        'week'
+      );
+    }
+
+    update(groupCallback, titleCallback, unit) {
+      const data = [];
+      for (const activity of groupCallback(this.activities)) {
+        data.push({
+          x: activity.date,
+          y: activity.distance,
+          activity: activity
         });
       }
 
-      const value = map.get(key);
-      value.time += activity.time;
-      value.distance += activity.distance;
-      value.elevation += activity.elevation;
-    }
-    return map.values();
-  }
-
-  function groupByDay(activities) {
-    return groupBy(activities, 'Y-w-D', date => date);
-  }
-
-  function groupByWeek(activities) {
-    return groupBy(activities, 'Y-w', date => {
-      return date.endOf('week');
-    });
-  }
-
-  function groupByMonth(activities) {
-    return groupBy(activities, 'Y-M', date => {
-      return date.endOf('month');
-    });
-  }
-
-  function titleDay(items, data) {
-    const date = data.datasets[items[0].datasetIndex].data[items[0].index].activity.date;
-    return date.format('MMM D, H:mm');
-  }
-
-  function titleWeek(items, data) {
-    const date = data.datasets[items[0].datasetIndex].data[items[0].index].activity.date;
-    return `${date.startOf('week').format('MMM D')} - ${date.endOf('week').format('MMM D')}`;
-  }
-
-  function titleMonth(items, data) {
-    const date = data.datasets[items[0].datasetIndex].data[items[0].index].activity.date;
-    return date.format('MMMM');
-  }
-
-  function update(group, title, unit) {
-    const years = new Map();
-    for (const activity of group(activities)) {
-      const year = activity.date.year();
-
-      if (!years.has(year)) {
-        years.set(year, []);
-      }
-      years.get(year).push({
-        x: activity.date,
-        y: activity.distance,
-        activity: activity
-      });
-    }
-
-    config.options.scales.xAxes.length = 0;
-    config.options.scales.xAxes.push({
-      type: 'time',
-      time: {
-        unit: unit,
-        unitStepSize: 1
-      }
-    });
-
-    config.options.tooltips.callbacks.title = title;
-
-    config.data.datasets.length = 0;
-    for (const [year, data] of years.entries()) {
-      config.data.datasets.push({
-        label: year,
-        borderColor: 'rgba(33, 150, 243, 1)',
-        backgroundColor: 'rgba(33, 150, 243, 0.1)',
-        borderWidth: 1,
-        lineTension: 0,
-        pointRadius: 5,
-        pointHitRadius: 5,
-        pointBorderColor: 'rgba(33, 150, 243, 1)',
-        pointBackgroundColor: 'rgb(233, 245, 254, 1)',
-        pointHoverRadius: 5,
-        data: data
-      });
-    }
-
-    if (chart) {
-      chart.update(config);
-    } else {
-      chart = new Chart(document.getElementById('chart').getContext('2d'), config);
-    }
-
-    chart.update(config);
-  }
-
-  async function fetch(url) {
-    return new Promise(resolve => {
-      const request = new XMLHttpRequest();
-
-      request.open('GET', url);
-      request.responseType = 'json';
-      request.onload = () => {
-        const activities = [];
-        for (const activity of request.response) {
-          if (activity.type !== 'Ride') {
-            continue;
-          }
-          activity.date = moment(activity.date);
-          activity.distance = activity.distance / 1000;
-          activities.push(activity);
+      this.config.options.scales.xAxes.length = 0;
+      this.config.options.scales.xAxes.push({
+        type: 'time',
+        time: {
+          unit: unit,
+          unitStepSize: 1
         }
-        resolve(activities);
+      });
+
+      this.config.options.tooltips.callbacks.title = (items, data) => {
+        const activity = data.datasets[items[0].datasetIndex].data[items[0].index].activity;
+        return titleCallback(activity);
+      };
+
+      this.config.data.datasets.length = 0;
+      this.config.data.datasets.push(
+        new Dataset('', data))
+
+      if (this.chart) {
+        this.chart.update(this.config);
+      } else {
+        this.chart = new Chart(document.getElementById('chart').getContext('2d'), this.config);
       }
-      request.send();
-    });
+    }
   }
 
-  async function init() {
-    moment.updateLocale('de', {
-      week: {
-        dow: 1  // Because weeks don't start on Sunday...
-      }
-    });
+  moment.updateLocale('de', {
+    week: {
+      dow: 1  // Because weeks don't start on Sunday...
+    }
+  });
 
-    activities = await fetch('/static/data/strava.json');
+  const app = new Application();
 
-    document.getElementById('btnDay').onclick = () => {
-      update(groupByDay, titleDay, 'week');
-    };
+  await app.init();
 
-    document.getElementById('btnWeek').onclick = () => {
-      update(groupByWeek, titleWeek, 'week');
-    };
+  document.getElementById('btnDay').onclick = () => {
+    app.update(
+      activities => activities.groupByDay(),
+      activity => activity.titleDay,
+      'week'
+    );
+  };
 
-    document.getElementById('btnMonth').onclick = () => {
-      update(groupByMonth, titleMonth, 'month');
-    };
+  document.getElementById('btnWeek').onclick = () => {
+    app.update(
+      activities => activities.groupByWeek(),
+      activity => activity.titleWeek,
+      'week'
+    );
+  };
 
-    update(groupByWeek, titleWeek, 'week');
-  }
-
-  init();
+  document.getElementById('btnMonth').onclick = () => {
+    app.update(
+      activities => activities.groupByMonth(),
+      activity => activity.titleMonth,
+      'month'
+    );
+  };
 })();
